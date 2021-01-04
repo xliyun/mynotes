61e4e79218b9c8ed77937d141e5e3334e641dcfb
17815 <yuntao.li@hand-china.com>
2021-01-04 16:43:44 +0800

    nio群聊位置变动

--- codebase/src/com/liyun/netty/nio/groupchat/GroupChatClient.java
+++ codebase/src/com/liyun/netty/nio/groupchat/GroupChatClient.java
@@ -1,10 +1,8 @@
-package com.liyun.netty.groupchat;
+package com.liyun.netty.nio.groupchat;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.nio.Buffer;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
