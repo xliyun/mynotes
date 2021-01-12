5f190614eed22645d2291d48aae1bfaf3e41178f
17815 <yuntao.li@hand-china.com>
2021-01-12 14:09:39 +0800

    netty dubbo rpc

---
+++ codebase/src/com/liyun/netty/dubborpc/provider/HelloServiceImpl.java
@@ -0,0 +1,25 @@
+package com.liyun.netty.dubborpc.provider;
+
+import com.liyun.netty.dubborpc.publicinterface.HelloService;
+
+/**
+ * @description:
+ * @author: xiaoliyu
+ * @date: 2021-01-10 19:34
+ */
+public class HelloServiceImpl implements HelloService {
+
+    private int count =0;
+
+    //当有消费方调用该方法时，就返回一个结果
+    @Override
+    public String hello(String mes) {
+        System.out.println("收到客户端消息"+mes);
+        //根据mes，返回不同的结果
+        if(mes !=null){
+            return "你好客户端，我已经收到你的消息 ["+mes+"] 第"+(++count)+"次收到消息";
+        }else{
+            return "你好客户端，我已经收到你的消息";
+        }
+    }
+}
